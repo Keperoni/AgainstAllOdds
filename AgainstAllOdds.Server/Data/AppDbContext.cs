0e2bb6360e05e6dc71ac17f0c629bb25332a0e79
Pedro Ferreira <NB30250@novabase.pt>
2026-03-08 15:52:17 +0000

    feat: add AppDbContext with Habits and HabitLogs DbSets and composite unique index

---
+++ AgainstAllOdds.Server/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using AgainstAllOdds.Server.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace AgaintsAllOdds.Server.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Habit> Habits { get; set; }
+		public DbSet<HabitLog> HabitLogs { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<HabitLog>()
+				.HasIndex(hl => new { hl.HabitId, hl.Date })
+				.IsUnique();
+		}
+	}
+}
